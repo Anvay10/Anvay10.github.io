4e200b91d5c2685f576761733708d275e5f412f0
Anvay10 <103751508+Anvay10@users.noreply.github.com>
2023-04-20 18:01:48 +0530

    Update and rename ecg.js to script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+// Set up the chart
+var ctx = document.getElementById('ecgChart').getContext('2d');
+var ecgChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'ECG Data',
+      data: [],
+      fill: false,
+      borderColor: 'rgb(255, 99, 132)',
+      tension: 0.1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
+
+// Set up WebSocket connection
+var socket = new WebSocket('ws://YOUR_IP_ADDRESS:YOUR_PORT_NUMBER');
+
+// When the connection is open, send a message to the server to request ECG data
+socket.onopen = function(event) {
+  socket.send('ECG data request');
+};
+
+// When a message is received from the server, update the chart with the new data
+socket.onmessage = function(event) {
+  var data = JSON.parse(event.data);
+  var ecg = data.ecg;
+  var timestamp = data.timestamp;
+
+  // Add the new data to the chart
+  ecgChart.data.labels.push(timestamp);
+  ecgChart.data.datasets[0].data.push(ecg);
+  ecgChart.update();
+};

d0fa82a51601e904dc8d728ab40ff19f30a5c61f
Anvay10 <103751508+Anvay10@users.noreply.github.com>
2023-04-20 14:28:14 +0530

    Update live_chart.js

--- live_chart.js
+++ live_chart.js
@@ -1,54 +1,51 @@
-const ecgChart = new Chart(document.getElementById('ecgChart'), {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'ECG',
-            data: [],
-            backgroundColor: 'rgba(0, 0, 255, 0.5)',
-            borderColor: 'rgba(0, 0, 255, 1)',
-            borderWidth: 1,
-            fill: false,
-        }],
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        animation: {
-            duration: 0,
-        },
-        scales: {
-            xAxes: [{
-                display: false,
-            }],
-            yAxes: [{
-                ticks: {
-                    suggestedMax: 1023,
-                    suggestedMin: 0,
-                },
-            }],
-        },
-    },
-});
-
-const ecgData = [];
-
-function updateChart(jsonData) {
-    const data = JSON.parse(jsonData).data;
-
-    for (let i = 0; i < data.length; i++) {
-        ecgData.push(data[i]);
-
-        if (ecgData.length > 400) {
-            ecgData.shift();
+// Define the chart configuration
+const chartConfig = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Data',
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+      data: []
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [{
+        type: 'realtime',
+        realtime: {
+          duration: 20000, // Total duration of the chart in milliseconds
+          refresh: 1000, // Refresh rate in milliseconds
+          delay: 1000, // Delay between the time an update is received and when it is displayed
+          onRefresh: function(chart) { // Callback function for updating the chart data
+            chart.data.datasets.forEach(function(dataset) {
+              dataset.data.push({
+                x: Date.now(),
+                y: Math.random() * 100 // Sample data point
+              });
+            });
+          }
         }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100
+        }
+      }]
+    },
+    plugins: {
+      streaming: {
+        frameRate: 30 // Number of frames per second
+      }
     }
+  }
+};
 
-    ecgChart.data.datasets[0].data = ecgData;
-    ecgChart.update();
-}
-
-const source = new EventSource('/events');
-source.addEventListener('data', (event) => {
-    updateChart(event.data);
-});
+// Get the canvas element and create the chart
+const chartElement = document.getElementById('chart');
+const chart = new Chart(chartElement, chartConfig);

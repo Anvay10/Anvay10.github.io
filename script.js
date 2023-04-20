0eea18dad643c62043faa0f87ce7517aaeb620cc
Anvay10 <103751508+Anvay10@users.noreply.github.com>
2023-04-20 17:16:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-var ws = new WebSocket("ws://" + window.location.hostname + ":81/");
-ws.onmessage = function(event) {
-  addDataToChart(parseInt(event.data));
-};
+// Create the Chart object
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'ECG Data',
+      data: [],
+      fill: false,
+      borderColor: 'rgb(255, 99, 132)',
+      tension: 0.1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+// Function to add data to the Chart object
+function addData(label, data) {
+  myChart.data.labels.push(label);
+  myChart.data.datasets.forEach((dataset) => {
+    dataset.data.push(data);
+  });
+  myChart.update();
+}

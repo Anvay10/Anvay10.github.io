8774cd5887b8295a3f8e12bea0292cf0c6146dcd
Anvay10 <103751508+Anvay10@users.noreply.github.com>
2023-04-20 17:30:48 +0530

    Update and rename script.js to ecg.js

---
+++ ecg.js
@@ -0,0 +1,40 @@
+var ctx = document.getElementById('ecgChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'ECG Data',
+      data: [],
+      fill: false,
+      borderColor: 'rgb(255, 99, 132)',
+      tension: 0.1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+function updateChart() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var ecgData = this.responseText.split(',');
+      ecgData.pop(); // Remove the last empty element
+      myChart.data.labels = Array.from(Array(ecgData.length).keys()).map(String);
+      myChart.data.datasets[0].data = ecgData;
+      myChart.update();
+    }
+  };
+  xhttp.open("GET", "/ecgData", true);
+  xhttp.send();
+}
+
+setInterval(updateChart, 100);

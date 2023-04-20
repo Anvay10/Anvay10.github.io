adb0897626eb279ff02dc24be1951fc31729d95b
Anvay10 <103751508+Anvay10@users.noreply.github.com>
2023-04-20 14:44:06 +0530

    Update live_chart.js

--- live_chart.js
+++ live_chart.js
@@ -1,4 +1,3 @@
-// Define the chart configuration
 const chartConfig = {
   type: 'line',
   data: {
@@ -12,8 +11,10 @@ const chartConfig = {
     }]
   },
   options: {
-    responsive: true,
+    responsive: false, // Set to false to disable automatic resizing of chart
     maintainAspectRatio: false,
+    height: 300, // Set height of chart
+    width: 500, // Set width of chart
     scales: {
       xAxes: [{
         type: 'realtime',
@@ -48,4 +49,6 @@ const chartConfig = {
 
 // Get the canvas element and create the chart
 const chartElement = document.getElementById('chart');
+chartElement.height = chartConfig.options.height; // Set the height of the chart element
+chartElement.width = chartConfig.options.width; // Set the width of the chart element
 const chart = new Chart(chartElement, chartConfig);
